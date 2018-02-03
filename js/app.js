0d9dcf4b590c0159ee50a7658b51cbc7fe6441b5
Jakub Wyczesany <ghaelios@gmail.com>
2018-02-03 15:34:12 +0100

    Creating board

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    var GameOfLife = function () {
-        this.boardWidth = 0;
-        this.boardHeight = 0;
+    var GameOfLife = function (x, y) {
+        this.boardWidth = x;
+        this.boardHeight = y;
+        this.board = document.getElementById("board");
+        this.createBoard = function() {
+            this.board.style.width = (this.boardWidth * this.boardWidth);
+            this.board.style.height = (this.boardHeight * this.boardHeight);
+            var boardSize = this.board.style.width * this.board.style.height;
+            for (var i=0; i<boardSize; i++) {
+                var newDiv = document.createElement("div");
+                this.board.appendChild(newDiv);
+            }
+        };
+        this.cells = [];
 
     };
+
 
     var game = new GameOfLife(10, 10);
 
     console.log(game);
+
 
 });
